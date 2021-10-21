5b62928dde962ccac578b18959bc72a88010438b
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2021-10-21 23:46:14 +0200

    init

---
+++ Blog/Blog/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+
+        List<BlogItem> items = new List<BlogItem>()
+        {
+            new BlogItem(){
+                Title="Programowanie w ASP.NET",
+                Content="ASP.NET – zbiór technologii opartych na frameworku zaprojektowanym przez firmę Microsoft. Przeznaczony jest do budowy różnorodnych aplikacji internetowych, a także aplikacji typu XML Web Services. ",
+                CreationTimestamp=DateTime.Now
+            },
+            new BlogItem(){
+                Title="Programowanie w C#",
+                Content="wieloparadygmatowy język programowania zaprojektowany w latach 1998–2001 przez zespół pod kierunkiem Andersa Hejlsberga dla firmy Microsoft.",
+                CreationTimestamp=DateTime.Now
+            }
+        };
+        public IActionResult Add(BlogItem item)
+        {
+            items.Add(item);
+            return View("ConfirmBlogItem", item);
+        }
+
+        public IActionResult List()
+        {
+            return View(items);
+        }
+    }
+}
---
+++ Blog/Blog/Models/BlogItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class BlogItem
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationTimestamp { get; set; }
+    }
+}
