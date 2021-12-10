9d1db99160f8f333ef174065a5d833ff4b360267
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2021-12-10 13:25:07 +0100

    5

--- Blog/Blog/Models/BlogItem.cs
+++ Blog/Blog/Models/BlogItem.cs
@@ -8,6 +8,83 @@ using System.Threading.Tasks;
 
 namespace Blog.Models
 {
+    public interface ICustomerBlogItemRepository
+    {
+        IList<BlogItem> FindByName(string namePattern);
+        IList<BlogItem> FindPage(int page, int size);
+        BlogItem FindById(int id);
+    }
+
+    class CustomerBlogItemRepository : ICustomerBlogItemRepository
+    {
+        private ApplicationDbContext context;
+        public CustomerBlogItemRepository(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+
+        public IList<BlogItem> FindByName(string namePattern)
+        {
+            return (from p in context.BlogItems where p.Title.Contains(namePattern) select p).ToList();
+        }
+
+        public IList<BlogItem> FindPage(int page, int size)
+        {
+            return (from p in context.BlogItems select p).OrderBy(p => p.Title).Skip((page - 1) * size).Take(size).ToList();
+        }
+        public BlogItem FindById(int id)
+        {
+            return context.BlogItems.Find(id);
+        }
+    }
+    public interface ICrudBlogItemRepository
+    {
+        BlogItem Find(int id);
+        BlogItem Delete(int id);
+        BlogItem Add(BlogItem blogItem);
+        BlogItem Update(BlogItem blogItem);
+        IList<BlogItem> FindAll();
+    }
+
+    class CrudBlogItemRepository : ICrudBlogItemRepository
+    {
+        private ApplicationDbContext _context;
+        public CrudBlogItemRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public BlogItem Find(int id)
+        {
+            return _context.BlogItems.Find(id);
+        }
+
+        public BlogItem Delete(int id)
+        {
+            var blogItem = _context.BlogItems.Remove(Find(id)).Entity;
+            _context.SaveChanges();
+            return blogItem;
+        }
+
+        public BlogItem Add(BlogItem blogItem)
+        {
+            var entity = _context.BlogItems.Add(blogItem).Entity;
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public BlogItem Update(BlogItem blogItem)
+        {
+            var entity = _context.BlogItems.Update(blogItem).Entity;
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public IList<BlogItem> FindAll()
+        {
+            return _context.BlogItems.ToList();
+        }
+    }
     public class EFBlogItemRepository : IBlogItemRepository
     {
         private ApplicationDbContext _applicationDbContext;
