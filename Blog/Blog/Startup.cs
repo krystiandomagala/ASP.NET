f7aca76a3f42dd0fa5d1e6e2ab179d20d7a7c11f
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-01-21 16:16:42 +0100

    lab9

--- Blog/Blog/Startup.cs
+++ Blog/Blog/Startup.cs
@@ -68,6 +68,7 @@ namespace Blog
 
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
